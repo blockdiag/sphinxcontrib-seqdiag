67ec6e15869e8f0519552ee716141366aac30711
tk0miya <i.tkomiya@gmail.com>
2015-03-25 11:41:41 +0900

    blockdiag: Remove setuptools from dependencies list (Fix #96)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-requires = ['blockdiag>=1.5.0', 'seqdiag>=0.9.3', 'Sphinx>=0.6', 'setuptools']
+requires = ['blockdiag>=1.5.0', 'seqdiag>=0.9.3', 'Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-seqdiag',
