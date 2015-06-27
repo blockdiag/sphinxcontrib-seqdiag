deda53c1a742b5c691fa65eb35a6829df2d4f474
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-06-27 16:52:45 +0900

    Repository is moved to blockdiag organization

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = ['blockdiag>=1.5.0', 'seqdiag>=0.9.3', 'Sphinx>=0.6']
 setup(
     name='sphinxcontrib-seqdiag',
     version='0.8.3',
-    url='http://github.com/tk0miya/sphinxcontrib-seqdiag',
+    url='http://github.com/blockdiag/sphinxcontrib-seqdiag',
     license='BSD',
     author='Takeshi KOMIYA',
     author_email='i.tkomiya@gmail.com',
