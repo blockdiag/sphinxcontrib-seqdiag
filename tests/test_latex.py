173a4e5be2e87ac067bf19c260f980b787b0e947
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-06-27 11:27:54 +0900

    Use posixpath.join() to generate relative URI

--- tests/test_latex.py
+++ tests/test_latex.py
@@ -158,7 +158,7 @@ class TestSphinxcontribSeqdiagLatex(unittest.TestCase):
         source = (app.outdir / 'test.tex').read_text(encoding='utf-8')
         self.assertRegexpMatches(source, '\\\\includegraphics{seqdiag-.*?.png}')
 
-        figure = re.compile('\\\\begin{figure}\\[htbp\\]\n\\\\centering.*?'
+        figure = re.compile('\\\\begin{figure}\\[htbp\\]\r?\n\\\\centering.*?'
                             '\\\\caption{hello world}\\\\end{figure}', re.DOTALL)
         self.assertRegexpMatches(source, figure)
 
