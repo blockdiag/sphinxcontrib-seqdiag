3ac2c0afbdfc9c3545591b2e6495cc97b73c10ee
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-12-01 23:58:06 +0900

    Update exclude rule for find_package()

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'Topic :: Utilities',
     ],
     platforms='any',
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     install_requires=requires,
     namespace_packages=['sphinxcontrib'],
